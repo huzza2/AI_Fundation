30d15efcab8c29718b41dbb73b0c0469fd96a8d0
Zhanghao Hu <zhanhu@paypal.com>
2022-11-12 20:37:31 -0800

    disable GPU

--- train.py
+++ train.py
@@ -10,8 +10,8 @@ import pathlib
 
 from keras_preprocessing.image import ImageDataGenerator
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(gpus[0], True)
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+# tf.config.experimental.set_memory_growth(gpus[0], True)
 
 
 def create_folder_structure():
